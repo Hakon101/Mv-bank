4cdce0de50fbda687b3d14a3bab741ba3f835055
Hakon101 <marcoantoniodavigomes2002@gmail.com>
2026-02-09 17:15:34 -0300

    Atualiza tabela RastreabilidadeEntity

--- src/main/java/br/edu/ifg/luziania/controllres/model/entity/RastreabilidadeEntity.java
+++ src/main/java/br/edu/ifg/luziania/controllres/model/entity/RastreabilidadeEntity.java
@@ -4,55 +4,46 @@ package br.edu.ifg.luziania.controllres.model.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
-
 @Entity
-@Table(name = "entity_rastreabilidade")
+@Table(name ="RastreabilidadeEntity")
 public class RastreabilidadeEntity {
 
+    //chave primaria
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Getter @Setter
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "rastreabilidade_seq")
     private Integer id;
 
     @Getter @Setter
-    private String usuario;
+    @Column(nullable = false)
+    private String tipo;
 
     @Getter @Setter
-    private String controller;
+    @Column(nullable = false)
+    private double valor;
 
     @Getter @Setter
-    private String endpoint;
+    @Column(nullable = false)
+    private String destinatario;
 
     @Getter @Setter
-    private String metodoHttp;
+    @Column(nullable = false)
+    private String remetente;
 
-    @Getter @Setter
-    private String ip;
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime hora;
 
-    @Getter @Setter
-    private String dataHora;
-
-    public RastreabilidadeEntity() {
+    protected RastreabilidadeEntity() {
     }
 
-    public RastreabilidadeEntity(
-            Integer id,
-            String usuario,
-            String controller,
-            String endpoint,
-            String metodoHttp,
-            String ip,
-            String dataHora
-    ) {
-        this.id = id;
-        this.usuario = usuario;
-        this.controller = controller;
-        this.endpoint = endpoint;
-        this.metodoHttp = metodoHttp;
-        this.ip = ip;
-        this.dataHora = dataHora;
+    public RastreabilidadeEntity(String tipo, double valor, String destinatario, String remetente) {
+        this.tipo = tipo;
+        this.valor = valor;
+        this.destinatario = destinatario;
+        this.remetente = remetente;
     }
 }
 
